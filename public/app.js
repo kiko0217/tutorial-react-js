ad5e4af7c92ad7d877c4585befb7cda70cbb8925
kiko0217 <kiko0217@github.com>
2022-06-24 05:54:32 +0700

    create example simple fetch data with async function

--- public/app.js
+++ public/app.js
@@ -2,9 +2,26 @@ const root = document.querySelector('#root');
 const rootReact = ReactDOM.createRoot(root);
 
 const App = () => {
-  const getData = fetch('https://api.spaceflightnewsapi.net/v3/blogs').then(res => res.json()).then(res => res).catch(err => console.log('error :' + err));
-  console.log(getData);
-  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Data Fetch"));
+  const [news, setNews] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
+
+  const getData = async () => {
+    const req = await fetch('https://api.spaceflightnewsapi.net/v3/blogs');
+    const res = await req.json(); // console.log(res)
+
+    setNews(res);
+    setTimeout(setLoading.bind(this, false), 3000);
+  };
+
+  getData();
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Data Fetch"), !loading ? /*#__PURE__*/React.createElement("ul", null, news.map(item => {
+    {
+      /* console.log(item) */
+    }
+    return /*#__PURE__*/React.createElement("li", {
+      key: item.title
+    }, item.title);
+  })) : /*#__PURE__*/React.createElement("h2", null, "Loading data ..."));
 };
 
 rootReact.render( /*#__PURE__*/React.createElement(App, null));
--- src/app.js
+++ src/app.js
@@ -2,14 +2,32 @@ const root = document.querySelector('#root')
 
 const rootReact = ReactDOM.createRoot(root);
 const App = () => {
-    const getData = fetch('https://api.spaceflightnewsapi.net/v3/blogs')
-    .then(res => res.json())
-    .then(res => res)
-    .catch(err => console.log('error :'+ err))
-    console.log(getData)
+    const [news, setNews] = React.useState([])
+    const [loading, setLoading] = React.useState(true)
+    const getData = async () =>{
+        const req = await fetch('https://api.spaceflightnewsapi.net/v3/blogs')
+        const res = await req.json()
+        // console.log(res)
+        setNews(res)
+        setTimeout(setLoading.bind(this, false),3000)
+    }
+    getData()
     return (
         <>
             <h1>Data Fetch</h1>
+            {
+                !loading ? (
+                    <ul>
+                        {
+                            news.map(item => {
+                                {/* console.log(item) */ }
+                                return <li key={item.title}>{item.title}</li>
+                            })
+                        }
+                    </ul>):(
+                        <h2>Loading data ...</h2>
+                    )
+            }
         </>
     )
 }
